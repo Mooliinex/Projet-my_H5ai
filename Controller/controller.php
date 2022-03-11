1eb2b91ddfe827fdeabdaf72f47cb7e6e4f743a8
fabien Le Fichoux <fabien.le-fichoux@epitech.eu>
2022-03-11 10:57:51 +0100

    Add scandir, diff des .img..., explode file

--- Controller/controller.php
+++ Controller/controller.php
@@ -4,12 +4,12 @@ class Controller {
     public $path; 
     public $path_org;
 
-
+    
     public function start($url){
         $this->urlCheck($url);
     }
 
-    public function setPath($path){
+    public function setCurrent($path){
         $this->path = $path;
     }
 
@@ -23,10 +23,56 @@ class Controller {
             if ($value !=''){
                 $path_serv.= DIRECTORY_SEPARATOR.$value;
             }
+            if (is_dir($path_serv)){
+                $this->path_org = 'folder';
+                $this->setCurrent($path_serv);
+            } else { 
+                if (is_file($path_serv)){
+                    if(preg_match('/([\w\-])+(.png|.pnj|.jpg|.jpeg|.svg|.gif|.ico)$/', $value)){
+                        $this->path_org = 'image';
+                        $this->setCurrent($path_serv);
+                    } else {
+                        $this->path_org = 'file';
+                        $this->setCurrentPath($path_serv);
+
+                    }
+                   
+                }
+                else {
+                    $errorpop = true;
+                    $pop .= '/'.$value;               
+                 }
+            }
+        } 
+        if ($errorpop == true) 
+            echo "<p> Impossible pour la recherche ".$message.".</p>";
+            $this->scan_dir();
+    
+} 
+public function scandir(){
+    $path_serv = str_replace($_SERVER['DOCUMENT_ROOT'],'',$this->path);
+    $stop= explode ('/', $path_serv);
+    array_pop($stop);
+    $undo = BASE_URI.implode(DIRECTORY_SEPARATOR, $undo);
+  
+    if($this->path_org == 'folder'){
+        $contents = scandir($this->path);
+        $stock_path = [];
+  
+        foreach($contents as $content){
+          if($this->path == $_SERVER['DOCUMENT_ROOT']){
+            if($content != '.' && $content != '..'){
+              $path = $path_serv.DIRECTORY_SEPARATOR.$content;
+              $stock_path[$path] = $content;
+            }
+          }
+          else {
+            if($content != '.' ){
+              $path = $path_serv.DIRECTORY_SEPARATOR.$content;
+              $stock_path[$path] = $content;
+            }
+          }
         }
-    }
-
-
-
-
+        $this->render($stock_path);
+      }
 }
